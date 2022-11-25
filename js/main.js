b1ca186b265cfd87c8e6a4478bb9edf1bc35ccd8
whgusxo <whgusxo789@gmail.com>
2022-11-25 21:36:08 +0900

    Create project

---
+++ js/main.js
@@ -0,0 +1,169 @@
+gsap.to('.title1-1', .3, {
+  delay: .2,
+  y: -2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-2', .3, {
+  delay: .4,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-3', .3, {
+  delay: .6,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-4', .3, {
+  delay: .8,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-5', .3, {
+  delay: 1,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-6', .3, {
+  delay: 1.2,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title1-7', .3, {
+  delay: 1.4,
+  y: -1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-1', .3, {
+  delay:1.6,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-2', .3, {
+  delay:1.8,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-3', .3, {
+  delay:2,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-4', .3, {
+  delay:2.2,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-5', .3, {
+  delay:2.4,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title2-6', .3, {
+  delay:2.6,
+  y: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title3-1', .5, {
+  delay: 2.8,
+  y: -2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title3-2', .5, {
+  delay: 3,
+  y: -2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.title3-3', .5, {
+  delay: 3,
+  y: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#hello', 3, {
+  delay: 3,
+  y:10,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#wink', 4, {
+  delay: 3,
+  x:10,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#coding', 4, {
+  delay: 3,
+  y:-10,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#star', 4, {
+  delay: 3,
+  y:10,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#vie', 4, {
+  delay: 3,
+  x:-10,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('#skull_star', 4, {
+  delay: 3,
+  y:-10,
+  x:5,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.circle2', 3, {
+  delay: 3,
+  scale: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
+gsap.to('.circle3', 3, {
+  delay: 6,
+  scale: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: Power1.easeInOut
+})
